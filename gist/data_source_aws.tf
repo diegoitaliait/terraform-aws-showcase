0e14ec4f07685b480e80fb5aef8023f8a5b391c2
dm.lagosmorales <dm.lagosmorales@almaviva.it>
2020-01-07 11:59:18 +0100

    added gist files

---
+++ gist/data_source_aws.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "amazon_linux_ami" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+data "aws_region" "current" {}
