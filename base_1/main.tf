e651f14d9b34ed031e4573ed5a8e0847ad00f06f
diegoitaliait <diegomauricio777@gmail.com>
2020-07-19 22:55:00 +0200

    minimun version of main.tf

--- base_1/main.tf
+++ base_1/main.tf
@@ -1,38 +1,5 @@
 provider "aws" {
-  version = "2.40.0"
-}
-
-# DATA
-
-data "aws_ami" "amazon_linux_ami" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name = "name"
-
-    values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
-    ]
-  }
-
-  filter {
-    name = "owner-alias"
-
-    values = [
-      "amazon",
-    ]
-  }
-}
-
-resource "aws_instance" "micro_base_test" {
-  ami           = data.aws_ami.amazon_linux_ami.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "micro_base_one"
-  }
+  version = "2.70.0"
 }
 
 resource "aws_sqs_queue" "terraform_queue" {
