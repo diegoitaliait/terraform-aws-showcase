b8272c0d397162832554f9780c41581cb04c089b
diegoitaliait <diegomauricio777@gmail.com>
2020-09-27 19:37:54 +0200

    output file updated

--- base_2/output.tf
+++ base_2/output.tf
@@ -1,3 +1,11 @@
-output "vm_id" {
-  value = aws_instance.vm.id
+output "vm_arn" {
+  value = aws_instance.vm.arn
+}
+
+output "environment" {
+  value = var.env
+}
+
+output "ec2_instance_name" {
+  value = local.ec2_instance_name
 }
